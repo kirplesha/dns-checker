c558514cf649665f994a7da6adedf0e622477893
Kirill <kirplesha@gmail.com>
2023-06-02 00:38:03 +0300

    Fixes

---
+++ src/DnsCheckerServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace kirplesha\DnsChecker;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class DnsCheckerServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
